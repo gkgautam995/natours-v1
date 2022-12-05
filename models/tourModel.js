fbf58ca5bc34d14643d52f15be422656fbcacf91
Gautam Kumar <gautam.engg.0556@gmail.com>
2022-12-05 18:29:29 +0530

    Updated package.json

--- models/tourModel.js
+++ models/tourModel.js
@@ -154,7 +154,7 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
-tourSchema.post(/^find/, function (docs, next) {
+tourSchema.post(/^find/, (docs, next) => {
   //console.log(`Query took ${Date.now() - this.start} milliseconds`);
   next();
 });
